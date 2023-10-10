6bb3884da29241bbc65aff422d782fe8d94c337b
dominikkorwek <dominikkorwek@interia.pl>
2023-10-10 18:03:02 +0200

    wejscie

--- najwieksza.java
+++ najwieksza.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 //zadanie 2.2
 public class najwieksza {
@@ -7,18 +7,24 @@ public class najwieksza {
 //wspisanie trzech liczb
         System.out.println("podaj mi 3 liczby, a posegreguje je rosnaco");
         double[] liczby = new double[3];
-        liczby[0] = 1;
-        liczby[1] = 2;
-        liczby[2] = 3;
+        Scanner skaner = new Scanner(System.in);
+        for(int i=0;i<3;i++)
+            liczby[i] = skaner.nextDouble();
+
 
 //sortowanie
-        for(int i = 0; i < 2; i++)
-            if (liczby[i] > liczby[i+1])
-            {
-                double temp = liczby[i];
-                liczby[i]=liczby[i+1];
-                liczby[i+1]= temp;
+        int zmiana = 1;
+        while(zmiana > 0){
+            zmiana = 0;
+            for(int i=0; i<2; i++){
+                if(liczby[i]< liczby[i+1]){
+                    double temp = liczby[i+1];
+                    liczby[i+1] = liczby[i];
+                    liczby[i] = temp;
+                    zmiana++;
+                }
             }
+        }
 //wypisanie liczb
         System.out.println("liczby zostaly posortowane: "+ liczby[0]+" > "+liczby[1]+" > "+liczby[2]);
     }
